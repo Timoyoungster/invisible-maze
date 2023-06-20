4dcecdc29f8958aa985d3ab9181bfea917483e5b
Timoyoungster <Timoyoungster@users.noreply.github.com>
2023-06-20 11:51:34 +0200

    trying random creation

--- maze_creation_test/maze_creation_test.js
+++ maze_creation_test/maze_creation_test.js
@@ -1,10 +1,48 @@
 
+        
 function setup() {
     createCanvas(400, 400);
+    
+    horizontal = [
+        [1,0,0,0,1],
+        [1,0,0,0,1],
+        [1,0,0,0,1],
+        [1,0,0,0,1]
+    ];
+    
+    vertical = [
+        [1,0,0,0,1],
+        [1,0,0,0,1],
+        [1,0,0,0,1],
+        [1,0,0,0,1]
+    ];
+    
+    generate_maze();
 }
 
 function draw() {
     background(0);
+    stroke(0, 255, 255);
+    strokeWeight(8);
+    for (var x = 0; x < 4;  x++) {
+        for (var y =0; y < 5; y++) {
+            if (horizontal[x][y] === 1) {
+                line(x * 100, y * 100, x * 100 + 100, y * 100);
+            }
+            if (vertical[x][y] === 1) {
+                line(y * 100, x * 100, y * 100, x * 100 + 100);
+            }
+        }
+    }
+}
+
+function generate_maze() {
+    for (var x = 0; x < 4; x++) {
+        for (var y = 0; y < 3; y++) {
+            horizontal[x][y + 1] = Math.round(Math.random());
+            vertical[x][y + 1] = Math.round(Math.random());
+        }
+    }
 }
 
 /* layout

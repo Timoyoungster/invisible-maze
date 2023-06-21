3c516c3c9a718fa9de1419f0564e24cf3a4951e3
Timoyoungster <Timoyoungster@users.noreply.github.com>
2023-06-21 11:12:27 +0200

    playing with variants of equal h and v bits

--- maze_creation_test/maze_creation_test.js
+++ maze_creation_test/maze_creation_test.js
@@ -37,8 +37,87 @@ function draw() {
 
 function generate_maze() {
     //c_random();
-    c_fromBytes();
+    //c_fromBytes();
+    //c_same();
+    c_same_with_flip(true);
+    //c_same_always_flip(true);
     redraw();
+}
+
+function c_same_always_flip(sliced_gen) {
+    let num;
+    let bits = "";
+    if (sliced_gen) {
+        for (var i = 0; i < 4; i++) {
+            num = getRandomInt(16, 31);
+            bits += (num).toString(2);
+        }
+    }
+    else {
+        num = getRandomInt(524288, 1048575);
+        bits = (num).toString(2);
+    }
+    
+    for (var x = 0; x < 4; x++) {
+        for (var y = 0; y < 5; y++) {
+            var bit = parseInt(bits[x * 5 + y]);
+            if (y === 0 || y === 4) {
+                bit = 1
+            }
+            horizontal[x][y] = bit;
+            if (!(y === 0 || y === 4)) {
+                bit = 1 - bit;
+            }
+
+            vertical[x][y] = bit;
+        }
+    }
+}
+
+function c_same_with_flip(sliced_gen) {
+    let num;
+    let bits = "";
+    if (sliced_gen) {
+        for (var i = 0; i < 4; i++) {
+            num = getRandomInt(16, 31);
+            bits += (num).toString(2);
+        }
+    }
+    else {
+        num = getRandomInt(524288, 1048575);
+        bits = (num).toString(2);
+    }
+    
+    for (var x = 0; x < 4; x++) {
+        let flip = getRandomInt(1,3);
+        for (var y = 0; y < 5; y++) {
+            var bit = parseInt(bits[x * 5 + y]);
+            if (y === 0 || y === 4) {
+                bit = 1
+            }
+            horizontal[x][y] = bit;
+            if (flip === y && !(y === 0 || y === 4)) {
+                bit = 1 - bit;
+            }
+            vertical[x][y] = bit;
+        }
+    }
+}
+
+function c_same() {
+    let num = getRandomInt(524288, 1048575);
+    let bits = (num).toString(2);
+    
+    for (var x = 0; x < 4; x++) {
+        for (var y = 0; y < 5; y++) {
+            var bit = parseInt(bits[x * 5 + y]);
+            if (y === 0 || y === 4) {
+                bit = 1
+            }
+            horizontal[x][y] = bit;
+            vertical[x][y] = bit;
+        }
+    }
 }
 
 function c_fromBytes() {
@@ -50,7 +129,7 @@ function c_fromBytes() {
         while (numh % prevh === 0) {
             numh = 2 * getRandomInt(8, 15) + 1;
         }
-        while (numv % prevh === 0) {
+        while (numv % prevv === 0) {
             numv = 2 * getRandomInt(8, 15) + 1;
         }
         let h = (numh).toString(2);
